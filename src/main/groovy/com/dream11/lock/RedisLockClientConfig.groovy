e4e48f06ae3db15e927c11cc508fa3f20d148e22
Yogesh Badke <yogesh.badke@dream11.com>
2025-09-23 17:36:16 +0530

    fix: remove redundant JsonProperty annotations

--- src/main/groovy/com/dream11/lock/RedisLockClientConfig.groovy
+++ src/main/groovy/com/dream11/lock/RedisLockClientConfig.groovy
@@ -1,15 +1,8 @@
 package com.dream11.lock
 
-import com.fasterxml.jackson.annotation.JsonProperty
-
 class RedisLockClientConfig implements LockClientConfig {
-    @JsonProperty("key")
     String key
-
-    @JsonProperty("host")
     String host
-
-    @JsonProperty("port")
     Integer port = 6379
 
     RedisLockClientConfig() {
