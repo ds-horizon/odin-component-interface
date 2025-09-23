ef6e1753f05772ec5e73c5ccf2eabb4176c2ab71
Yogesh Badke <yogesh.badke@dream11.com>
2025-09-23 17:36:16 +0530

    fix: remove redundant JsonProperty annotations

--- src/main/groovy/com/dream11/state/S3StateClientConfig.groovy
+++ src/main/groovy/com/dream11/state/S3StateClientConfig.groovy
@@ -1,17 +1,11 @@
 package com.dream11.state
 
 import com.fasterxml.jackson.annotation.JsonInclude
-import com.fasterxml.jackson.annotation.JsonProperty
 
 class S3StateClientConfig implements StateClientConfig {
-    @JsonProperty("uri")
     String uri
-
-    @JsonProperty("endpoint")
     @JsonInclude(JsonInclude.Include.NON_NULL)
     String endpoint
-
-    @JsonProperty("region")
     String region
 
     S3StateClientConfig() {
--- src/main/groovy/com/dream11/state/StateConfig.groovy
+++ src/main/groovy/com/dream11/state/StateConfig.groovy
@@ -1,16 +1,14 @@
 package com.dream11.state
 
-import com.fasterxml.jackson.annotation.JsonProperty
+
 import com.fasterxml.jackson.annotation.JsonSubTypes
 import com.fasterxml.jackson.annotation.JsonTypeInfo
 
 class StateConfig {
-    @JsonProperty("provider")
     String provider
 
     @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXTERNAL_PROPERTY, property = "provider")
     @JsonSubTypes([@JsonSubTypes.Type(value = S3StateClientConfig.class, name = "S3")])
-    @JsonProperty("config")
     StateClientConfig config
 
     StateConfig() {
